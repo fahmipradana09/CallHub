962fef3e32c3e444ff09b92a8490a6b3f9aca0f4
FahmiPradana <fahmipradama@gmail.com>
2023-04-03 14:16:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/callhub/DashboardBarberShop.kt
@@ -0,0 +1,11 @@
+package com.example.callhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardBarberShop : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_barber_shop)
+    }
+}
